a0a2bfa9f02d10af838df285d3e99554ce4a2be6
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-04-28 12:39:39 +0500

    Started using Twig pack

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     // homepage controller
     #[Route('/')]
     public function index(): Response
     {
-        return new Response('<strong>Starsho</strong> - is RUNNING!');
+        $starshipCount = 457;
+
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+        
+
+        return $this->render('main/index.html.twig', [
+            'numOfStarships' => $starshipCount,
+            'myShip' => $myShip
+        ]);
     }
 }
